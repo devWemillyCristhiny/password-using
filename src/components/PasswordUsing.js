34c321733c3dca76091c74fe0bd917234cb8a88a
devWemillyCristhiny <devwemilly.frontend@gmail.com>
2020-12-09 21:57:13 -0300

    terminado

--- src/components/PasswordUsing.js
+++ src/components/PasswordUsing.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 export default function(){
     return (
-        <div>
-            oi
+        <div className="inputBox">
+            <input type="password" name="" placeholder="Enter Password" id="password"/>
+            <div id="toggle" onClick="showHide();"></div>
+            <script type="text/javascript">
+                const password = document.getElementById('password');
+                const toggle = document.getElementById('toggle');
+
+                function showHide(){
+                    if (password.type === 'password'){
+                        password.setAttribute('type', 'text');
+                        toggle.classList.add('hide')
+                    }
+                    else {
+                        password.setAttribute('type', 'password');
+                        toggle.classList.remove(hide)
+                    }
+                }
+            </script>
         </div>
     )
 }
